377adf6195fa2a91dc6b9ae0419b614137964918
otaviozaia <36265898+otaviozaia@users.noreply.github.com>
2018-12-20 02:42:43 -0200

    teste

--- projetoLMS/lms/LMSbasico/LMSbasico/settings.py
+++ projetoLMS/lms/LMSbasico/LMSbasico/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://system-vereda-for-coord.herokuapp.com/']
+ALLOWED_HOSTS = ['https://system-vereda.herokuapp.com']
 
 
 # Application definition
